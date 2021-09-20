248c2527e1a8d2f9032a6e919edeb34f27a85820
xupypg88 <mikhail.topskiy@veeam.com>
2021-09-20 20:06:40 +0300

    added policy creating function and json templates for data

--- main.py
+++ main.py
@@ -18,48 +18,62 @@ class VBA:
         if self.token == None:
             data = {"grant_type": "password", "username": self.creds['login'], "password": self.creds['pass']}
             r = requests.post(self.url+'/token', data=data, verify=False)
-            self.headers = {"Authorization": "Bearer {0}".format(json.loads(r.content)['access_token'])}
+            self.token = json.loads(r.content)['access_token']
+            self.headers = {"Authorization": "Bearer {0}".format(self.token), 'Content-type': 'application/json', 'x-api-version': self.api_version}
+
         pass
 
     def getRepositories(self):
         self.getAccessToken()
-        r = requests.get(self.url+'/repositories', data=self.data, headers=self.headers, verify=False)
+        r = requests.get(self.url+'/repositories', headers=self.headers, verify=False)
         return json.loads(str(r.text))['results']
 
     def getAccounts(self):
         self.getAccessToken()
-        r = requests.get(self.url + '/accounts/amazon', data=self.data, headers=self.headers, verify=False)
+        r = requests.get(self.url + '/accounts/amazon', headers=self.headers, verify=False)
         return json.loads(str(r.text))['results']
 
     def getBuckets(self):
         self.getAccessToken()
-        r = requests.get(self.url + '/cloudInfrastructure/buckets', data=self.data, headers=self.headers, verify=False)
+        r = requests.get(self.url + '/cloudInfrastructure/buckets',  headers=self.headers, verify=False)
         return json.loads(str(r.text))['results']
 
     def getS3Folders(self, bucket_id):
         self.getAccessToken()
-        r = requests.get(self.url + '/cloudInfrastructure/buckets/{0}/folders'.format(bucket_id), data=self.data, headers=self.headers, verify=False)
+        r = requests.get(self.url + '/cloudInfrastructure/buckets/{0}/folders'.format(bucket_id), headers=self.headers, verify=False)
         return json.loads(str(r.text))['results']
 
-    def addRepository(self, account_id, name, bucket_id, folder, encryption = False):
+    def getBackupPolicies(self):
         self.getAccessToken()
-        data = {"x-api-version": self.api_version,
-                "amazonAccountId": account_id,
+        r = requests.get(self.url + '/policies', headers=self.headers, verify=False)
+        return json.loads(str(r.text))['results']
+
+
+    def addRepository(self, account_id, name, bucket_id, path, encryption = False):
+        self.getAccessToken()
+        data = {"amazonAccountId": account_id,
                 "name": name,
                 "amazonBucketId": bucket_id,
-                "amazonStorageFolder": folder,
+                "amazonStorageFolder": path,
                 "enableEncryption": encryption}
-        r = requests.post(self.url + '/repositories', data=data, headers=self.headers, verify=False)
-        pass
+        r = requests.post(self.url + '/repositories', json=data, headers=self.headers, verify=False)
+        return json.loads(str(r.text))
+
+    def addBackupPolicy(self):
+        self.getAccessToken()
+        data = {}
+        with open('json-templates/aws-automation-policy-linux.json', 'r') as fp:
+            data = json.loads(fp.read())
+        r = requests.post(self.url + '/policies', json=data, headers=self.headers, verify=False)
+        return json.loads(str(r.text))
 
 vb = VBA('18.116.163.28','11005','grizzly','7ujMko0admin!')
+print(vb.addBackupPolicy())
 
-#vb.getRepositories()
-
-
-for bucket in vb.getBuckets():
-    if 'mtop-s3-lab' in bucket['name']:
-        for folder in vb.getS3Folders(bucket['id']):
-            if 'awx' in folder['name']:
-                vb.addRepository(vb.getAccounts()[0]['id'], "Test Repo", bucket['id'], 'awx')
+def addrepo(vb):
+    for bucket in vb.getBuckets():
+        if 'mtop-s3-lab' in bucket['name']:
+        #for folder in vb.getS3Folders(bucket['id']):
+        #    if 'awx-automation2' in folder['name']:
+            vb.addRepository(vb.getAccounts()[0]['id'], "main-repository-awx-automation", bucket['id'], 'awx-automation')
 
